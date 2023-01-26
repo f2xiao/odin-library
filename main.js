3b3cee8be3642f254329961141776037170cd567
f2xiao <fxiao.mecheng@gmail.com>
2023-01-25 22:20:59 -0500

    Add displayBooks function

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: 'Alice in Wonder Land',
+    author: 'Tim Burton',
+    pages: 123,
+    read: false
+  },
+  {
+    title: 'The Three-Body Problem',
+    author: 'Yuxi Liu',
+    pages: 200,
+    read: false
+  },
+  {
+    title: 'Harry Potter',
+    author: ' J. K. Rowling',
+    pages: 600,
+    read: false
+  },
+];
+
+const container = document.querySelector('#container');
 
 function Book() {
   
@@ -8,3 +29,16 @@ function addBookToLibrary() {
   
 }
 
+function displayBooks() {
+  // loop through 'myLibrary' array to perform the following operations:
+  // create a 'li' element
+  // add textContent to it
+  // append the 'li' element to the container ele
+  myLibrary.forEach(book => {
+    const node = document.createElement('li');
+    node.textContent = `${book.title} by ${book.author} has ${book.pages} pages, and is ${book.read ? 'read' : 'not read yet'}`;
+    container.appendChild(node);
+  })
+}
+
+displayBooks();
